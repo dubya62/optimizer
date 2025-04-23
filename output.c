604d8df238346c097bf805ea24add82f9eb9fdb7
dubya <dubya297@yahoo.com>
2025-04-23 14:27:52 -0500

    Added a 'slow' C program to optimize

--- output.c
+++ output.c
@@ -1,5 +1,5 @@
 #include "stdio.h"
 struct TestStruct { int a ; } ; 
  int add ( int var1 , int var2 ) { int var10 = var1 + var2 ; return var10 ; } int main ( int var4 , char * * var5 ) { struct TestStruct var6 ; label_0: 
- if ( 1 ) { goto label_1 ; } else { } label_1: 
- var6.a = 2 ; var12 = 2 ; var13 = 3 ; var14 = "%d\n" ; var15 = int var8 ; var16 = var12 ; var17 = var13 ; var8 = add ( var16 , var17 ) ; printf ( var14 , var15 ) ; ; int var11 = var8 ; return var11 ; }
+ if ( 1 ) { goto label_1 ; goto label_0 ; } else { } label_1: 
+ var6.a = 2 ; int var8 = add ( 2 , 3 ) ; ; printf ( "%d\n" , var8 ) ; ; int var11 = var8 ; return var11 ; }
